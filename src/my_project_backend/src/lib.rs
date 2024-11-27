24920010324e3231b114b14aa8469b35d7392cf3
Eduard Muntianov <u96_edumun_waw@technischools.com>
2024-11-27 10:56:24 +0000

    dziala

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,17 +1,19 @@
 use std::cell::RefCell;
-thread_local!{
-    static MSG: RefCell<String>=RefCell::new(String::new());
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
 }
+
 #[ic_cdk::update]
-fn save_msg(msg: String){
-    MSG.with(|static_msg| *static_msg.borrow_mut()=msg);
-
+fn save_msg(msg: String) {
+    CHAT.with(|static_msg| static_msg.borrow_mut().push(msg));
 }
+
 #[ic_cdk::query]
-fn get_msg()->String{
-    MSG.with(|static_msg| static_msg.borrow().clone())
-
+fn get_chat() -> Vec<String> {
+    CHAT.with(|static_msg| static_msg.borrow().clone())
 }
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
